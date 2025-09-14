021f8a8c3919b41bce9c177b63104ebaced07434
corneleaa <kornelijadam0909@gmail.com>
2025-09-14 18:08:42 +0300

    Initial Commit

---
+++ projektas/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  projektas
+//
+//  Created by Kornelija Dambrauskaitė on 14/09/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
