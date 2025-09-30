0e066762a08374f0fcbc44c1d13f896d6cace706
corneleaa <kornelijadam0909@gmail.com>
2025-09-30 09:44:20 +0200

    Create student.h
    pridejau struktura studentas ir bibliotekas i .h faila

---
+++ student.h
@@ -0,0 +1,42 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <limits>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+
+using cout;
+using cin;
+using endl;
+using string;
+using vector;
+using setw;
+using left;
+using right;
+using fixed;
+using setprecision;
+using getline;
+using numeric_limits;
+using streamsize;
+using ifstream;
+using ofstream;
+using istringstream;
+
+struct Studentas {
+    string vardas;
+    string pavarde;
+    vector<int> pazymiai;
+    int egzaminas{};
+    double galutinis{};
+};
+
+
+
