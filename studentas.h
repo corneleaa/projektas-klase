e404cfdcff62434be5ff6ff26f9af3c92732fa6b
corneleaa <kornelijadam0909@gmail.com>
2025-09-30 09:52:09 +0200

    Update studentas.h

--- studentas.h
+++ studentas.h
@@ -1,34 +1,12 @@
-#ifndef STUDENT_H
-#define STUDENT_H
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
 
 #include <iostream>
-#include <iomanip>
 #include <string>
 #include <vector>
-#include <limits>
-#include <cstdlib>
-#include <ctime>
-#include <algorithm>
-#include <fstream>
-#include <sstream>
-#include <chrono>
 
-using cout;
-using cin;
-using endl;
 using string;
 using vector;
-using setw;
-using left;
-using right;
-using fixed;
-using setprecision;
-using getline;
-using numeric_limits;
-using streamsize;
-using ifstream;
-using ofstream;
-using istringstream;
 
 struct Studentas {
     string vardas;
