97232750ce3f3a4842cc6ef6dc210c5a1aee2d54
corneleaa <kornelijadam0909@gmail.com>
2025-10-01 13:43:51 +0200

    Update studentas.cpp

--- studentas.cpp
+++ studentas.cpp
@@ -1,7 +1,40 @@
-//
-//  student.cpp
-//  
-//
-//  Created by Kornelija Dambrauskaitė on 30/09/2025.
-//
+#include "studentas.h"
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+
+using ofstream;
+using ifstream;
+using istringstream;
+using cerr;
+using endl;
+using setw;
+using left;
+using right;
+using fixed;
+using setprecision;
+
+double skaiciuotiVidurki(const vector<int>& v) {
+    if (v.empty()) return 0.0;
+    double suma = 0;
+    for (int x : v) suma += x;
+    return suma / v.size();
+}
+
+double skaiciuotiMediana(vector<int> v) {
+    if (v.empty()) return 0.0;
+    std::sort(v.begin(), v.end());
+    size_t n = v.size();
+    if (n % 2 == 0)
+        return (v[n/2 - 1] + v[n/2]) / 2.0;
+    else
+        return v[n/2];
+}
+
+double skaiciuotiGalutini(const vector<int>& paz, int egz) {
+    return skaiciuotiVidurki(paz) * 0.4 + egz * 0.6;
+}
 
