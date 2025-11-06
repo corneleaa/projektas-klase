112adc53ae50d2c039ed88786159d5af89271109
corneleaa <kornelijadam0909@gmail.com>
2025-11-06 02:51:07 +0200

    Update studentas.h

--- studentas.h
+++ studentas.h
@@ -17,56 +17,56 @@ inline double mediana_ref(const std::vector<int>& v) {
     std::vector<int> tmp = v;
     return mediana(tmp);
 }
+class Studentas {
+private:
+    std::string vardas_;
+    std::string pavarde_;
+    std::vector<int> nd_;
+    int egzaminas_;
+    double galutinis_;
 
-struct Studentas {
-    string vardas;
-    string pavarde;
-    vector<int> pazymiai;
-    int egzaminas{};
-    double galutinis{};
+public:
+    Studentas();
+    Studentas(std::istream& is);
+    ~Studentas();
+
+    inline std::string vardas() const { return vardas_; }
+    inline std::string pavarde() const { return pavarde_; }
+    inline double galutinis() const { return galutinis_; }
+
+    inline void setVardas(const std::string& v) { vardas_ = v; }
+    inline void setPavarde(const std::string& p) { pavarde_ = p; }
+    inline void setEgzaminas(int e) { egzaminas_ = e; }
+    inline void setNd(const std::vector<int>& v) { nd_ = v; }
+
+    std::istream& readStudent(std::istream& is);
+    double skaiciuotiGalutini(double (*f)(const std::vector<int>&)) const;
+    void perskaiciuoti(double (*f)(const std::vector<int>&));       // atnaujina galutinis_
+    void spausdinti(std::ostream& os) const;
 };
 
-double skaiciuotiVidurki(const vector<int>& v);
-double skaiciuotiMediana(vector<int> v);
-double skaiciuotiGalutini(const vector<int>& paz, int egz);
-double skaiciuotiGalutiniPagalTipa(const vector<int>& paz, int egz, int tipas);
+bool comparePagalVarda(const Studentas&, const Studentas&);
+bool comparePagalPavarde(const Studentas&, const Studentas&);
+bool comparePagalGalutini(const Studentas&, const Studentas&);
 
-Studentas generuotiStudenta(int id);
-void generuotiFaila(const string& failoVardas, int kiek);
-
-vector<Studentas> nuskaitytiIsFailo(const string& failoVardas);
-void rikiuotiStudentus(vector<Studentas>& grupe, const string& pagal);
-void padalintiStudentusTik(vector<Studentas>& grupe, vector<Studentas>& vargs, vector<Studentas>& kiet);
-void isvestiStudentus(const vector<Studentas>& grupe, const string& failoVardas);
-
-void split_strat1_vector(const std::vector<Studentas>& src,
-                         std::vector<Studentas>& vargs,
-                         std::vector<Studentas>& kiet);
-
-void split_strat2_vector(std::vector<Studentas>& all,
-                         std::vector<Studentas>& vargs);
-
-void split_strat3_vector(std::vector<Studentas>& all,
-                         std::vector<Studentas>& vargs);
-
-void split_strat1_list(const std::list<Studentas>& src,
-                       std::list<Studentas>& vargs,
-                       std::list<Studentas>& kiet);
-
-void split_strat2_list(std::list<Studentas>& all,
-                       std::list<Studentas>& vargs);
-
-void split_strat3_list(std::list<Studentas>& all,
-                       std::list<Studentas>& vargs);
+std::vector<Studentas> nuskaitytiIsFailo(const std::string& failas);
 
 template <typename Container>
-Container nuskaitytiIsFailoT(const string& failoVardas);
-
-template <typename Container>
-void padalintiStudentusTikT(const Container& grupe, Container& vargs, Container& kiet);
-
-template <typename Container>
-void isvestiStudentusT(const Container& grupe, const string& failoVardas);
-
-#endif
-
+Container nuskaitytiIsFailoT(const std::string& failas) {
+    Container grupe;
+    std::ifstream in(failas);
+    if (!in) {
+        std::cerr << "Nepavyko atidaryti failo: " << failas << std::endl;
+        return grupe;
+    }
+    std::string eil;
+    std::getline(in, eil); // header
+    while (std::getline(in, eil)) {
+        if (eil.empty()) continue;
+        std::istringstream iss(eil);
+        Studentas s(iss);
+        grupe.emplace_back(std::move(s));
+    }
+    std::cout << "Nuskaityta studentų: " << grupe.size() << std::endl;
+    return grupe;
+}
