751dece294a43f98ee4b88757090033583e49a92
corneleaa <kornelijadam0909@gmail.com>
2025-11-06 02:49:05 +0200

    Update studentas.cpp

--- studentas.cpp
+++ studentas.cpp
@@ -30,87 +30,40 @@ void Studentas::spausdinti(std::ostream& os) const {
        << std::setw(10) << std::fixed << std::setprecision(2) << galutinis_ << "\n";
 }
 
-void generuotiFaila(const string& failoVardas, int kiek) {
-    ofstream out(failoVardas);
-    if (!out) {
-        cerr << "Nepavyko sukurti failo: " << failoVardas << endl;
-        return;
-    }
+bool comparePagalVarda(const Studentas& a, const Studentas& b) { return a.vardas() < b.vardas(); }
+bool comparePagalPavarde(const Studentas& a, const Studentas& b) { return a.pavarde() < b.pavarde(); }
+bool comparePagalGalutini(const Studentas& a, const Studentas& b) { return a.galutinis() < b.galutinis(); }
+
+std::vector<Studentas> nuskaitytiIsFailo(const std::string& failas) {
+    return nuskaitytiIsFailoT<std::vector<Studentas>>(failas);
+}
+void isvestiStudentus(const std::vector<Studentas>& grupe, const std::string& failas) {
+    isvestiStudentusT(grupe, failas);
+}
+Studentas generuotiStudenta(int id) {
+    Studentas s;
+    s.setVardas("Vardas" + std::to_string(id));
+    s.setPavarde("Pavarde" + std::to_string(id));
+    std::vector<int> nd;
+    nd.reserve(5);
+    for (int i = 0; i < 5; ++i) nd.push_back(1 + rand() % 10);
+    s.setNd(nd);
+    s.setEgzaminas(1 + rand() % 10);
+    s.perskaiciuoti(vidurkis);
+    return s;
+}
+void generuotiFaila(const std::string& failoVardas, int kiek) {
+    std::ofstream out(failoVardas);
     out << "Vardas Pavarde ND1 ND2 ND3 ND4 ND5 Egzaminas\n";
-    for (int i = 1; i <= kiek; i++) {
+    for (int i = 1; i <= kiek; ++i) {
         Studentas s = generuotiStudenta(i);
-        out << s.vardas << " " << s.pavarde << " ";
-        for (int paz : s.pazymiai) out << paz << " ";
-        out << s.egzaminas << "\n";
+        out << s.vardas() << " " << s.pavarde() << " ";
+        for (int j = 0; j < 5; ++j) out << (rand() % 10 + 1) << " ";
+        out << (rand() % 10 + 1) << "\n";
     }
 }
+static inline bool isVargs(const Studentas& s) { return s.galutinis() < 5.0; }
 
-vector<Studentas> nuskaitytiIsFailo(const string& failoVardas) {
-    vector<Studentas> grupe;
-    ifstream in(failoVardas);
-    if (!in) {
-        cerr << "Nepavyko atidaryti failo: " << failoVardas << endl;
-        return grupe;
-    }
-    string eilute;
-    getline(in, eilute);
-    while (getline(in, eilute)) {
-        if (eilute.empty()) continue;
-
-        if (!eilute.empty() && eilute.back() == '\r')
-            eilute.pop_back();
-        std::replace(eilute.begin(), eilute.end(), '\t', ' ');
-
-        istringstream iss(eilute);
-        Studentas s;
-        iss >> s.vardas >> s.pavarde;
-
-        vector<int> paz;
-        int pazymys;
-        while (iss >> pazymys)
-            paz.push_back(pazymys);
-        if (!paz.empty()) {
-            s.egzaminas = paz.back();
-            paz.pop_back();
-            s.pazymiai = paz;
-            s.galutinis = skaiciuotiGalutini(s.pazymiai, s.egzaminas);
-            grupe.push_back(s);
-        }
-    }
-    cout << " Nuskaityta studentų: " << grupe.size() << endl;
-    return grupe;
-}
-void rikiuotiStudentus(vector<Studentas>& grupe, const string& pagal) {
-    if (pagal == "vardas")
-        std::sort(grupe.begin(), grupe.end(), [](auto& a, auto& b){ return a.vardas < b.vardas; });
-    else if (pagal == "pavarde")
-        std::sort(grupe.begin(), grupe.end(), [](auto& a, auto& b){ return a.pavarde < b.pavarde; });
-    else
-        std::sort(grupe.begin(), grupe.end(), [](auto& a, auto& b){ return a.galutinis < b.galutinis; });
-}
-void padalintiStudentusTik(vector<Studentas>& grupe, vector<Studentas>& vargs, vector<Studentas>& kiet) {
-    for (auto& s : grupe) {
-        if (s.galutinis < 5.0) vargs.push_back(s);
-        else kiet.push_back(s);
-    }
-}
-void isvestiStudentus(const vector<Studentas>& grupe, const string& failoVardas) {
-    ofstream out(failoVardas);
-    if (!out) {
-        cerr << "Nepavyko sukurti išvesties failo: " << failoVardas << endl;
-        return;
-    }
-    out << setw(15) << left << "Vardas"
-        << setw(15) << left << "Pavarde"
-        << setw(10) << right << "Galutinis\n";
-    for (auto& s : grupe)
-        out << setw(15) << left << s.vardas
-            << setw(15) << left << s.pavarde
-            << setw(10) << fixed << setprecision(2) << s.galutinis << "\n";
-}
-bool yraVargsiukas(const Studentas& s) {
-    return s.galutinis < 5.0;
-}
 void split_strat1_vector(const std::vector<Studentas>& src,
                          std::vector<Studentas>& vargs,
                          std::vector<Studentas>& kiet) {
@@ -118,9 +71,29 @@ void split_strat1_vector(const std::vector<Studentas>& src,
     std::partition_copy(src.begin(), src.end(),
                         std::back_inserter(vargs),
                         std::back_inserter(kiet),
-                        yraVargsiukas);
+                        isVargs);
 }
 
+void split_strat2_vector(std::vector<Studentas>& all,
+                         std::vector<Studentas>& vargs) {
+    vargs.clear();
+    auto mid = std::partition(all.begin(), all.end(), isVargs);
+    std::move(all.begin(), mid, std::back_inserter(vargs));
+    all.erase(all.begin(), mid);
+}
+
+void split_strat3_vector(std::vector<Studentas>& all,
+                         std::vector<Studentas>& vargs) {
+    vargs.clear();
+    vargs.reserve(all.size() / 2);
+    std::remove_copy_if(all.begin(), all.end(),
+                        std::back_inserter(vargs),
+                        [](const Studentas& s){ return !isVargs(s); });
+    auto it = std::remove_if(all.begin(), all.end(), isVargs);
+    all.erase(it, all.end());
+    all.shrink_to_fit();
+    vargs.shrink_to_fit();
+}
 void split_strat1_list(const std::list<Studentas>& src,
                        std::list<Studentas>& vargs,
                        std::list<Studentas>& kiet) {
@@ -128,131 +101,21 @@ void split_strat1_list(const std::list<Studentas>& src,
     std::partition_copy(src.begin(), src.end(),
                         std::back_inserter(vargs),
                         std::back_inserter(kiet),
-                        yraVargsiukas);
-}
-void split_strat2_vector(std::vector<Studentas>& all,
-                         std::vector<Studentas>& vargs) {
-    vargs.clear();
-    auto mid = std::partition(all.begin(), all.end(), yraVargsiukas);
-    std::move(all.begin(), mid, std::back_inserter(vargs));
-    all.erase(all.begin(), mid);
+                        isVargs);
 }
 
 void split_strat2_list(std::list<Studentas>& all,
                        std::list<Studentas>& vargs) {
     vargs.clear();
-    for (auto it = all.begin(); it != all.end();) {
-        if (yraVargsiukas(*it)) {
+    for (auto it = all.begin(); it != all.end(); ) {
+        if (isVargs(*it)) {
             auto cur = it++;
             vargs.splice(vargs.end(), all, cur);
         } else ++it;
     }
 }
-void split_strat3_vector(std::vector<Studentas>& all,
-                         std::vector<Studentas>& vargs) {
-    vargs.clear();
-    vargs.reserve(all.size() / 2);
-    std::remove_copy_if(all.begin(), all.end(),
-                        std::back_inserter(vargs),
-                        [](const Studentas& s){ return s.galutinis >= 5.0; });
 
-    auto it = std::remove_if(all.begin(), all.end(),
-                             [](const Studentas& s){ return s.galutinis < 5.0; });
-    all.erase(it, all.end());
-
-    all.shrink_to_fit();
-    vargs.shrink_to_fit();
-}
-void split_strat3_vector_stable(std::vector<Studentas>& all,
-                                std::vector<Studentas>& vargs) {
-    vargs.clear();
-    vargs.reserve(all.size() / 2);
-
-    auto mid = std::stable_partition(all.begin(), all.end(),
-                                     [](const Studentas& s){ return s.galutinis < 5.0; });
-
-    vargs.insert(vargs.end(),
-                 std::make_move_iterator(all.begin()),
-                 std::make_move_iterator(mid));
-
-    all.erase(all.begin(), mid);
-}
 void split_strat3_list(std::list<Studentas>& all,
                        std::list<Studentas>& vargs) {
-    vargs.clear();
-    for (auto it = all.begin(); it != all.end();) {
-        if (it->galutinis < 5.0) {
-            auto moveIt = it++;
-            vargs.splice(vargs.end(), all, moveIt);
-        } else ++it;
-    }
+    split_strat2_list(all, vargs);
 }
-template <typename Container>
-Container nuskaitytiIsFailoT(const string& failoVardas) {
-    Container grupe;
-    ifstream in(failoVardas);
-    if (!in) {
-        cerr << "Nepavyko atidaryti failo: " << failoVardas << endl;
-        return grupe;
-    }
-
-    string eilute;
-    getline(in, eilute);
-
-    while (getline(in, eilute)) {
-        if (eilute.empty()) continue;
-
-        if (!eilute.empty() && eilute.back() == '\r')
-            eilute.pop_back();
-
-        std::replace(eilute.begin(), eilute.end(), '\t', ' ');
-
-        istringstream iss(eilute);
-        Studentas s;
-        iss >> s.vardas >> s.pavarde;
-
-        vector<int> paz;
-        int pazymys;
-        while (iss >> pazymys)
-            paz.push_back(pazymys);
-
-        if (!paz.empty()) {
-            s.egzaminas = paz.back();
-            paz.pop_back();
-            s.pazymiai = paz;
-            s.galutinis = skaiciuotiGalutini(s.pazymiai, s.egzaminas);
-            grupe.emplace_back(std::move(s));
-        }
-    }
-
-    cout << " Nuskaityta studentų: " << grupe.size() << endl;
-    return grupe;
-}
-template <typename Container>
-void padalintiStudentusTikT(const Container& grupe, Container& vargs, Container& kiet) {
-    for (const auto& s : grupe) {
-        if (s.galutinis < 5.0) vargs.emplace_back(s);
-        else kiet.emplace_back(s);
-    }
-}
-template <typename Container>
-void isvestiStudentusT(const Container& grupe, const string& failoVardas) {
-    ofstream out(failoVardas);
-    if (!out) {
-        cerr << "Nepavyko sukurti išvesties failo: " << failoVardas << endl;
-        return;
-    }
-    out << setw(15) << left << "Vardas"
-        << setw(15) << left << "Pavarde"
-        << setw(10) << right << "Galutinis\n";
-    for (const auto& s : grupe)
-        out << setw(15) << left << s.vardas
-            << setw(15) << left << s.pavarde
-            << setw(10) << fixed << setprecision(2) << s.galutinis << "\n";
-}
-template vector<Studentas> nuskaitytiIsFailoT<vector<Studentas>>(const string&);
-template list<Studentas> nuskaitytiIsFailoT<list<Studentas>>(const string&);
-template void padalintiStudentusTikT<vector<Studentas>>(const vector<Studentas>&, vector<Studentas>&, vector<Studentas>&);
-template void padalintiStudentusTikT<list<Studentas>>(const list<Studentas>&, list<Studentas>&, list<Studentas>&);
-template void isvestiStudentusT<vector<Studentas>>(const vector<Studentas>&, const string&);
-template void isvestiStudentusT<list<Studentas>>(const list<Studentas>&, const string&);
